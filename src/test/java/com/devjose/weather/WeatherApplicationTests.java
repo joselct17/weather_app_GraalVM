6773eb2a55f1fe14d5540383cb989a602bf6c5c4
joselct17 <joselct17@gmail.com>
2025-08-28 14:36:49 +0200

    init app

---
+++ src/test/java/com/devjose/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjose.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
